1c1e251e90867ce7f646f293c7f2e2a0e15b1bfa
AChorlton <71873454+AChorlton@users.noreply.github.com>
2021-03-11 18:50:14 +0000

    Added new class 'DistanceConverter3' Added 'Conversion factors' to class

--- ConsoleAppProject/App01/DistanceUnits.cs
+++ ConsoleAppProject/App01/DistanceUnits.cs
@@ -27,4 +27,24 @@ namespace ConsoleAppProject.App01
     /// <author>
     /// Alex Chorlton
     /// </author>
+    
+    class DistanceConverter3
+    {
+        // Conversion Factors
+        public const int FeetInMiles = 5280;
+        public const double MetresInMiles = 1609.34;
+        public const double KilometresInMiles = 1.60934;
+
+        public const double FeetInKilometres = 3280.84;
+        public const double MetresInKilometres = 1000;
+        public const double MilesInKilometres = 0.621371;
+
+        public const double FeetInMetres = 1 / MetresInFeet;
+        public const double KilometersInMetres = 1 / MetresInKilometres;
+        public const double MilesInMetres = 1 / MetresInMiles;
+
+        public const double MetresInFeet = 0.3048;
+        public const double KilometresInFeet = 1 / FeetInKilometres;
+        public const double MilesInFeet = 1.0 / FeetInMiles;
+    }
 }
