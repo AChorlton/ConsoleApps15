18a70b83380353af750fd7786181fd736ff433c7
AChorlton <71873454+AChorlton@users.noreply.github.com>
2021-04-22 20:28:14 +0100

    Program.cs:
    Added in the choices for the user to chose which application that they want to go into.
    Added a promt that would appear to encourage the user to make a choice "Please chose an application..."
    
    DistanceConverter:
    
    Added in the summary for the application.
    
    Added in the Distance converter class which contains the distance variables and the conversion values for those variables.

---
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using ConsoleAppProject.Helpers;
+
+namespace ConsoleAppProject.App01
+{
+    /// <summary>
+    /// This is an application to convert the distance of one unit to another
+    /// It will provide a cue for the user to chose a distance in one unit (fromUnit) to
+    /// be converted into another unit (toUnit)
+    /// 
+    /// The units that can be converted are shown below.
+    /// 1. Miles
+    /// 2. Meters
+    /// 3. Feet
+    /// </summary>
+    /// 
+    /// <author>
+    /// Alex Chorlton
+    /// </author>
+
+    public class DistanceConverter
+    {
+        // Setup the distance variables
+        public DistanceUnits FromUnit { get; set; }
+        public DistanceUnits ToUnit { get; set; }
+        public double FromDistance { get; set; }
+        public double ToDistance { get; set; }
+        
+
+        // Conversions
+        public const int FEET_IN_MILES = 5280;
+        public const double FEET_IN_METRES = 3.28084;
+        public const double METRES_IN_MILES = 1609.34;
+
+        public DistanceConverter()
+        {
+            FromUnit = DistanceUnits.Miles;
+            ToUnit = DistanceUnits.Feet;
+        }
--- ConsoleAppProject/Program.cs
+++ ConsoleAppProject/Program.cs
@@ -1,17 +1,58 @@
 ﻿using ConsoleAppProject.App01;
+using ConsoleAppProject.App02;
+using ConsoleAppProject.App03;
+using ConsoleAppProject.App04;
+using ConsoleAppProject.Helpers;
+using System;
 
 namespace ConsoleAppProject
 {
-
     /// <summary>
-    /// This class runs first and create a DistanceConverter
-    /// allowing the user three ways of converting distances
-    /// </summary>
-    /// <author>
+    /// The method in this class is called first
+    /// when the application is started.  It will be for
+    /// applications 1 to 4 for CO453 CW1.
+    /// 
+    /// This Project has been modified by:
     /// Alex Chorlton
-    /// </author>
+    /// </summary>
     public static class Program
     {
-        
+        public static void Main(string[] args)
+        {
+            ConsoleHelper.OutputHeading("CO453 C# Programming - MAIN MENU", 0.2);
+            string[] choices = {
+                "APP01: Distance Converter",
+                "APP02: BMI Calculator",
+                "APP03: Student Grades System",
+                "APP04: Social Network"
+            };
+            int choice = ConsoleHelper.SelectChoice(choices);
+
+            if (choice == 1)
+            {
+                DistanceConverter converter = new DistanceConverter();
+                converter.ConvertDistance();
+            }
+            if (choice == 2)
+            {
+                BMI bmi = new BMI();
+                bmi.MainMenu();
+            }
+            if (choice == 3)
+            {
+                StudentGrades studentGrades = new StudentGrades();
+                studentGrades.OutputHeading();
+            }
+            if (choice == 4)
+            {
+                NetworkApp networkApp = new NetworkApp();
+                networkApp.DisplayMenu();
+            }
+            else
+            {
+                Console.WriteLine("Please chose an application...");
+            }
+
+        }
     }
 }
