ea89c72405ac05762bb1d4597e8a6ec5d2b33d0a
AChorlton <71873454+AChorlton@users.noreply.github.com>
2021-04-22 20:10:03 +0100

    .

--- ConsoleAppProject/Program.cs
+++ ConsoleAppProject/Program.cs
@@ -12,44 +12,6 @@ namespace ConsoleAppProject
     /// </author>
     public static class Program
     {
-        public static NetworkUI Network { get; set; }
-
-        public static void Main()
-        {
-            Use.OutputHeading(" C# Console Applications 2020");
-
-            string [] choices = { 
-                "App01: Distance Converter", "Quit" };
-
-            int choiceNo = Use.SelectChoice(choices);
-
-            if (choiceNo == 1)
-            {
-                DistanceConverter1 converter = new DistanceConverter1();
-                converter.ConvertDistance();
-            }
-            else if (choiceNo == 2)
-            {
-                BMI bmi = new BMI();
-                bmi.CalculateIndex();
-            }
-            else if (choiceNo == 3)
-            {
-                StudentGrades app = new StudentGrades();
-                app.OutputMenu();
-            }
-            else if (choiceNo == 4)
-            {
-                NetworkUI network = new NetworkUI();
-                network.DisplayMenu();
-            }
-            else if (choiceNo == 5)
-            {
-                GameView view = new GameView();
-
-                view.PlayGame();
-            }
-
-        }
+        
     }
 }
