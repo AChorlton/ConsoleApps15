62611ac93563a1e949130f219a09397fba9403d0
AChorlton <71873454+AChorlton@users.noreply.github.com>
2021-04-23 01:14:15 +0100

    added a header which will create a user interface to display the data to the user

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -39,6 +39,39 @@ namespace ConsoleAppProject.App01
         }
 
         /// <summary>
+        /// Ask the user for the unit in which they want to convert into another.
+        /// </summary>
+        /// <param name="prompt"></param>
+        /// <returns></returns>
+        private DistanceUnits SelectUnit(string prompt)
+        {
+            string choice = DisplayChoices(prompt);
+            DistanceUnits unit = ExecuteChoice(choice);
+            return unit;
+        }
+        private DistanceUnits ExecuteChoice(string choice)
+        {
+            //used to store local choice of unit
+            DistanceUnits unit;
+
+            switch (choice)
+            {
+                case "1": unit = DistanceUnits.Feet; break;
+                case "2": unit = DistanceUnits.Metres; break;
+                case "3": unit = DistanceUnits.Miles; break;
+                default: unit = DistanceUnits.NoUnit; break;
+            }
+
+            if (unit == DistanceUnits.NoUnit)
+            {
+                Console.WriteLine("You are unable to perform this action...");
+                Console.WriteLine("You must select a digit between 1 and 3");
+            }
+            Console.WriteLine($"\n You have selected: {unit}");
+            return unit;
+        }
+
+        /// <summary>
         /// Shows the results of the conversion of the units to the user.
         /// </summary>
         public void OutputDistance()
@@ -56,18 +89,6 @@ namespace ConsoleAppProject.App01
             FromDistance = InputDistance($" Please enter the number of {FromUnit}: ");
             CalculateDistance();
             OutputDistance();
-        }
-
-        /// <summary>
-        /// Ask the user for the unit in which they want to convert into another.
-        /// </summary>
-        /// <param name="prompt"></param>
-        /// <returns></returns>
-        private DistanceUnits SelectUnit(string prompt)
-        {
-            string choice = DisplayChoices(prompt);
-            DistanceUnits unit = ExecuteChoice(choice);
-            return unit;
         }
 
         /// <summary>
@@ -100,5 +121,50 @@ namespace ConsoleAppProject.App01
                 ToDistance = FromDistance / METRES_IN_MILES;
             }
         }
+
+        private static string DisplayChoices(string prompt)
+        {
+            Console.WriteLine();
+            Console.WriteLine($"1. {DistanceUnits.Feet}");
+            Console.WriteLine($"2. {DistanceUnits.Metres}");
+            Console.WriteLine($"3. {DistanceUnits.Miles}");
+            Console.WriteLine();
+
+            Console.Write(prompt);
+            string choice = Console.ReadLine();
+            return choice;
+        }
+
+        /// <summary>
+        /// General purpose imput method
+        /// Ask the user to input a distance unit
+        /// Input the unit as a double
+        /// </summary>
+        private double InputDistance(string prompt)
+        {
+            Console.Write(prompt);
+            string value = Console.ReadLine();
+            return Convert.ToDouble(value);
+        }
+
+        /// <summary>
+        /// Output header to the user
+        /// </summary> 
+        private void OutputHeader()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine();
+            Console.WriteLine("-=-=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-");
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("     Convert units of distance      ");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("         by Alex Chorlton           ");
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("-=-=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-");
+        }
+
+
+    }
+}
 
         
