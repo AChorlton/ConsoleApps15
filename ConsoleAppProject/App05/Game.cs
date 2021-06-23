a639e585a99e81047274812809c10e66ddf3dad5
AChorlton <71873454+AChorlton@users.noreply.github.com>
2021-06-23 17:03:53 +0100

    added in the final code for the computer being able to chose its moves for the game. The classes 'makecomputerchoice' is one of these moves

--- ConsoleAppProject/App05/Game.cs
+++ ConsoleAppProject/App05/Game.cs
@@ -27,3 +27,67 @@ namespace ConsoleAppProject.App05
         public Player Winner { get; set; }
 
         private Random generator = new Random(55);
+      
+        public Game(string name)
+        {
+            Human = new Player(name, GamePlayers.Player);
+            Computer = new Player("Computer", GamePlayers.Computer);
+
+            Round = 0;
+        }
+
+        public void Start()
+        {
+            Round = 1;
+            LastRound = 5;
+
+            CurrentPlayer = Human;
+
+            Human.Score = 0;
+            Computer.Score = 0;
+        }
+        public void MakeComputerChoice()
+        {
+            int choice = generator.Next(1, 4);
+
+            switch (choice)
+            {
+                case 1: Computer.Choice = GameChoices.Paper; break;
+                case 2: Computer.Choice = GameChoices.Rock; break;
+                case 3: Computer.Choice = GameChoices.Scissors; break;
+            }
+        }
+
+        public void ScoreRound()
+        {
+            if (Human.Choice == GameChoices.Rock &&
+               Computer.Choice == GameChoices.Paper)
+            {
+                Computer.Score += 2;
+                Winner = Computer;
+            }
+
+            // 5 other combinations to add
+
+            if (Round < LastRound)
+            {
+                Round++;
+            }
+            else End();
+        }
+
+        public void End()
+        {
+            if (Computer.Score > Human.Score)
+            {
+                Winner = Computer;
+            }
+            else if (Computer.Score < Human.Score)
+            {
+                Winner = Human;
+            }
+            else Winner = null;
+        }
+    }
+}
+
