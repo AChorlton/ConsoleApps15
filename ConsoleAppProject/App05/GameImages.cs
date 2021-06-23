18d54d0d9e96bf29d78e3135e94cde4f5c6e4a1f
AChorlton <71873454+AChorlton@users.noreply.github.com>
2021-06-23 16:21:50 +0100

    Created the the game images which will be displayed when the user choses their option e.g. rock or paper or scissors

--- ConsoleAppProject/App05/GameImages.cs
+++ ConsoleAppProject/App05/GameImages.cs
@@ -1,1 +1,106 @@
-﻿
+﻿using System;
+
+namespace ConsoleAppProject.App05
+{
+    /// <summary>
+    /// This class has a set of method that will draw out
+    /// some useful game images using textual characters
+    /// for Rock-Paper-Scissors game
+    /// </summary>
+    /// <author>
+    /// Alex Chorlton
+    /// 100174372
+    /// </author>
+    public static class GameImages
+    {
+        public static void DrawPaper(int x, int y)
+        {
+            Console.SetCursorPosition(x, y++);    // set start position then increment y to move down
+            Console.Write("      .--.------------------.");
+            Console.SetCursorPosition(x, y++);
+            Console.Write("     /      \\  \\ \\ \\ \\ \\ \\ \\ \\");
+            Console.SetCursorPosition(x, y++);
+            Console.Write("    /   OOO  \\                |");
+            Console.SetCursorPosition(x, y++);
+            Console.Write("   |   OOOO   ||               | |");
+            Console.SetCursorPosition(x, y++);
+            Console.Write("   |   OOOO   |                |");
+            Console.SetCursorPosition(x, y++);
+            Console.Write("    \\   OOO   /                /");
+            Console.SetCursorPosition(x, y++);
+            Console.Write("     \\      // / / / / / / / //");
+            Console.SetCursorPosition(x, y++);
+            Console.Write("       `--'-|| | | | | | | | |");
+            Console.SetCursorPosition(x, y++);
+            Console.Write("             \\                \\");
+            Console.SetCursorPosition(x, y++);
+            Console.Write("              \\                \\");
+            Console.SetCursorPosition(x, y++);
+            Console.Write("               \\                \\");
+            Console.SetCursorPosition(x, y++);
+            Console.Write("                \\ \\ \\ \\ \\ \\ \\ \\ \\\\");
+            Console.SetCursorPosition(x, y);
+            Console.Write("                 \\________________\\");
+            Console.WriteLine();
+        }
+
+        public static void DrawScissors(int x, int y)
+        {
+            Console.SetCursorPosition(x, y++);   // set start position then increment y to move down
+            Console.Write("     \\            /");
+            Console.SetCursorPosition(x, y++);
+            Console.Write("      \\          /");
+            Console.SetCursorPosition(x, y++);
+            Console.Write("       \\        /");
+            Console.SetCursorPosition(x, y++);
+            Console.Write("        \\      /");
+            Console.SetCursorPosition(x, y++);
+            Console.Write("         \\    /");
+            Console.SetCursorPosition(x, y++);
+            Console.Write("          \\  /");
+            Console.SetCursorPosition(x, y++);
+            Console.Write("           **");
+            Console.SetCursorPosition(x, y++);
+            Console.Write("          /  \\");
+            Console.SetCursorPosition(x, y++);
+            Console.Write("    (----/    \\----)");
+            Console.SetCursorPosition(x, y++);
+            Console.Write("     \\  /      \\  /");
+            Console.SetCursorPosition(x, y);
+            Console.Write("      ==        ==");
+            Console.WriteLine("\n\n");
+        }
+
+        public static void DrawRock(int x, int y)
+        {
+            Console.SetCursorPosition(x, y++);   // set start position then increment y to move down
+            Console.Write("                 ___---___     ");
+            Console.SetCursorPosition(x, y++);
+            Console.Write("              .--         --.    ");
+            Console.SetCursorPosition(x, y++);
+            Console.Write("           ./   ()       .-. \\.   ");
+            Console.SetCursorPosition(x, y++);
+            Console.Write("           /   o    .   (   )  \\  ");
+            Console.SetCursorPosition(x, y++);
+            Console.Write("          / .            '-'    \\  ");
+            Console.SetCursorPosition(x, y++);
+            Console.Write("         /     ()   ()           \\ ");
+            Console.SetCursorPosition(x, y++);
+            Console.Write("        |    o           ()       | ");
+            Console.SetCursorPosition(x, y++);
+            Console.Write("        |      .--.           O   | ");
+            Console.SetCursorPosition(x, y++);
+            Console.Write("         \\ .  |    |              |  ");
+            Console.SetCursorPosition(x, y++);
+            Console.Write("          \\   `.__.'     o   .   /    ");
+            Console.SetCursorPosition(x, y++);
+            Console.Write("           `\\  o    ()         /'    ");
+            Console.SetCursorPosition(x, y++);
+            Console.Write("              `--___    ___--'    ");
+            Console.SetCursorPosition(x, y);
+            Console.Write("                     ---         ");
+            Console.WriteLine();
+        }
+    }
+}
+
