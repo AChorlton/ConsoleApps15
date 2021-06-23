43b40a2f9316f921ce13a83e264c42d91bfd9b51
AChorlton <71873454+AChorlton@users.noreply.github.com>
2021-06-23 15:51:31 +0100

    Added in the play class which will prompt the player to start the game. The class additionally controlls the sequence of events during gameplay and keep them in a repetitive cycle until the game is ended

--- ConsoleAppProject/App05/GameView.cs
+++ ConsoleAppProject/App05/GameView.cs
@@ -1,1 +1,44 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+    /// <author>
+    /// Alex Chorlton
+    /// 100174372
+    /// </author>
+
+namespace ConsoleAppProject.App05
+{
+    public class GameView
+    {
+        private Game game;
+
+        /// <summary>
+        /// Controls the events in eacj round of
+        /// the game, and repeats until the game is ended by the player
+        /// </summary>
+        public void Play()
+        {
+            bool wantToQuit = true;
+
+            do
+            {
+                StartGame();
+                
+                GetPlayerChoice();
+                DisplayChoice(game.Human.Choice);
+                
+                game.MakeComputerChoice();
+                DisplayChoice(GameChoices.Scissors);
+
+                game.ScoreRound();
+
+                if(game.Round == game.LastRound)
+                {
+                    Console.WriteLine();
+
+                }
+
+            } while (!wantToQuit);
+        }
+
