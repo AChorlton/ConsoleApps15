4968172f3d5fe3548fd524a81e5a85e9a5a85117
AChorlton <71873454+AChorlton@users.noreply.github.com>
2021-06-23 15:35:43 +0100

    Added the option for users to input their name Added score keeping added player types and game choices

---
+++ ConsoleAppProject/App05/Player.cs
@@ -0,0 +1,28 @@
+﻿docusing System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppProject.App05
+{
+    /// <summary>
+    /// This is where the user will input their name allowing them to 
+    /// </summary>
+    public class Player
+    {
+        public string Name { get; }
+
+        public int Score { get; set; }
+
+        public GamePlayers PlayerType { get; } 
+
+        public GameChoices Choice { get; set; }
+
+        public Player(string name, GamePlayers playerType)
+        {
+            this.Name = name;
+            this.PlayerType = playerType;
+            Score = 0;
+            Choice = GameChoices.None;
+        }
+    }
+}
