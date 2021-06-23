9a5d8a410dcc69ab9f31d8477e0a95605e63d510
AChorlton <71873454+AChorlton@users.noreply.github.com>
2021-06-23 15:56:35 +0100

    Added in the player choices into GamePlayers.cs

--- ConsoleAppProject/App05/GamePlayers.cs
+++ ConsoleAppProject/App05/GamePlayers.cs
@@ -1,1 +1,16 @@
-﻿
+﻿namespace ConsoleAppProject.App05
+
+    /// <author>
+    /// Alex Chorlton
+    /// 100174372
+    /// </author>
+    /// <summary>
+    /// This class contains the different player choices within the game
+    /// </summary>
+
+{
+    public enum GamePlayers
+    {
+        None, Computer, Player
+    }
+}
